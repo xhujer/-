bdd9fd2778f927b4ca8cb09d506a77cddc508f21
晚安宝 <8006557@gmail.com>
2026-08-16 14:55:00 +0800

    更新 V2EX.js

--- V2EX.js
+++ V2EX.js
@@ -3,7 +3,7 @@ var COOKIE_KEY = "V2EX_Cookie";
 var COMMON_HEADERS = {
   "Accept": "*/*",
   "Accept-Language": "en,zh-CN;q=0.9,zh;q=0.8",
-  "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+  "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1",
   "Referer": "https://www.v2ex.com/"
 };
 
